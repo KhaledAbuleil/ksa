70d8b386ba3b7d86e7675f1ba62d963056c26bd3
saeednamih <saeednamih@gmail.com>
2015-06-04 12:23:16 +0300

    mainpage

--- G17_assignment3/src/MainPageGUI/MainPageGUI.java
+++ G17_assignment3/src/MainPageGUI/MainPageGUI.java
@@ -3,6 +3,8 @@ package MainPageGUI;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
 
 
 public class MainPageGUI extends JFrame {
@@ -10,49 +12,50 @@ public class MainPageGUI extends JFrame {
 		getContentPane().setLayout(null);
 		
 		JPanel Adminpanel = new JPanel();
-		Adminpanel.setBounds(545, 10, 143, 447);
+		Adminpanel.setBounds(571, 94, 117, 363);
 		getContentPane().add(Adminpanel);
 		Adminpanel.setLayout(null);
 		
 		JButton btnEditGroup = new JButton("Edit Group");
-		btnEditGroup.setBounds(38, 77, 95, 23);
+		btnEditGroup.setBounds(10, 93, 95, 23);
 		Adminpanel.add(btnEditGroup);
 		
 		JButton btnEditUser = new JButton("Edit User");
-		btnEditUser.setBounds(38, 127, 101, 23);
+		btnEditUser.setBounds(10, 147, 101, 23);
 		Adminpanel.add(btnEditUser);
 		
 		JButton btnSendMessage = new JButton("Send Message");
-		btnSendMessage.setBounds(38, 179, 101, 23);
+		btnSendMessage.setBounds(10, 198, 101, 23);
 		Adminpanel.add(btnSendMessage);
 		
 		JPanel userpanel = new JPanel();
-		userpanel.setBounds(10, 10, 533, 81);
+		userpanel.setBounds(20, 11, 678, 81);
 		getContentPane().add(userpanel);
 		userpanel.setLayout(null);
 		
 		JButton btnEditFolder = new JButton("Edit Folder");
-		btnEditFolder.setBounds(109, 11, 83, 23);
+		btnEditFolder.setBounds(10, 11, 83, 23);
 		userpanel.add(btnEditFolder);
 		
-		JButton btnCreatefile = new JButton("Create File");
-		btnCreatefile.setBounds(205, 11, 85, 23);
-		userpanel.add(btnCreatefile);
-		
-		JButton btnUpdateFile = new JButton("Update File");
-		btnUpdateFile.setBounds(300, 11, 87, 23);
-		userpanel.add(btnUpdateFile);
-		
 		JButton btnSendRequest = new JButton("Send Request");
-		btnSendRequest.setBounds(409, 11, 99, 23);
+		btnSendRequest.setBounds(235, 11, 111, 23);
 		userpanel.add(btnSendRequest);
 		
-		JButton btnAddFolder = new JButton("Add folder");
-		btnAddFolder.setBounds(10, 11, 89, 23);
-		userpanel.add(btnAddFolder);
+		JButton btnEditFile = new JButton("Edit File");
+		btnEditFile.setBounds(117, 11, 89, 23);
+		userpanel.add(btnEditFile);
+		
+		JLabel lblUsername = new JLabel("username");
+		lblUsername.setBounds(478, -3, 200, 50);
+		userpanel.add(lblUsername);
+		lblUsername.setVisible(false);
+		
+		JPanel MessagePanel = new JPanel();
+		MessagePanel.setBounds(20, 162, 548, 295);
+		getContentPane().add(MessagePanel);
 		
 		JPanel DenamicPanel = new JPanel();
-		DenamicPanel.setBounds(20, 102, 517, 355);
+		DenamicPanel.setBounds(20, 94, 548, 69);
 		getContentPane().add(DenamicPanel);
 	}
 
